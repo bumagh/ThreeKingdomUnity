4c5f44ffbf617fd3e9a5f715c64016a758be2123
akgstudio <bumaya1024@gmail.com>
2024-11-15 14:07:06 +0800

    场景切换

---
+++ Assets/Game/Scripts/LoginController.cs
@@ -0,0 +1,17 @@
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoginController : MonoBehaviour
+{
+    private Button startBtn;
+    void Awake()
+    {
+        startBtn = GameObject.Find("Canvas/StartBtn").GetComponent<Button>();
+        startBtn.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene("Game");
+        });
+    }
+}
