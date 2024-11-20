7ce689a48b159fa7682f829a12d7dbcab9fbe89a
akgstudio <bumaya1024@gmail.com>
2024-11-20 13:51:17 +0800

    优化skill

--- Assets/Game/Scripts/Game/Model/Skill.cs
+++ Assets/Game/Scripts/Game/Model/Skill.cs
@@ -2,70 +2,20 @@
 public class Skill
 {
     public int SkillId;
-    public string SkillMacro;
-    public string SkillTemp;
-    public float AtkCoe;
-    public int EnergyCost;
+    public string SkillName;
     public string Description;
-    public int Duration;
-    public int CdTime;
+    public float AtkCoe;
+    public int MpCost;
+    public int RoundCount;
 
     /// <summary>
     ///
-    ///0	自身技能
-    ///1	单体选择
-    ///2	范围攻击（点）选择
-    ///3	友方单位
+    // 0	自身单体
+    // 1	敌方单体
+    // 2	敌方多人
+    // 3	友方单体
+    // 4	友方多人
     /// </summary>
     public int SelectType;
-    public int SelectRange;
-    public int DamageRange;
-    public float Repulsion;
-    public int SliiTrigger;
-    public string TriggerNum;
-    public string Sound;
-    public int Bulletlogic;
-    public string BulletImg;
-    public string SkillIcon;
-
-    public Skill(
-        int skillId,
-        string skillMacro,
-        string skillTemp,
-        float atkCoe,
-        int energyCost,
-        string description,
-        int duration,
-        int cdTime,
-        int selectType,
-        int selectRange,
-        int damageRange,
-        float repulsion,
-        int sliiTrigger,
-        string triggerNum,
-        string sound,
-        int bulletlogic,
-        string bulletImg,
-        string skillIcon
-    )
-    {
-        SkillId = skillId;
-        SkillMacro = skillMacro;
-        SkillTemp = skillTemp;
-        AtkCoe = atkCoe;
-        EnergyCost = energyCost;
-        Description = description;
-        Duration = duration;
-        CdTime = cdTime;
-        SelectType = selectType;
-        SelectRange = selectRange;
-        DamageRange = damageRange;
-        Repulsion = repulsion;
-        SliiTrigger = sliiTrigger;
-        TriggerNum = triggerNum;
-        Sound = sound;
-        Bulletlogic = bulletlogic;
-        BulletImg = bulletImg;
-        SkillIcon = skillIcon;
-    }
+    public int SelectCount;
 }
